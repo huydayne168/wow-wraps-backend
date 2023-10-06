ee0d3143e428a74c568edb30848d807c1469fce2
Nguyễn Việt Huy <120047501+huydayne168@users.noreply.github.com>
2023-10-06 16:08:59 +0700

    Update allowedOrigins.js

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    "https://admin-wow-wraps-tsx.vercel.app/",
-    "https://client-wow-wraps-tsx.vercel.app/",
+    "https://admin-wow-wraps-tsx.vercel.app",
+    "https://client-wow-wraps-tsx.vercel.app",
     "http://localhost:3000",
     "http://localhost:3001",
 ];
