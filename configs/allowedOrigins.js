e0af08a991811ec68b39a7d359451fcbc01fcc97
huydayne <huynguyenviet1608@gmail.com>
2023-10-31 19:11:38 +0700

    add new admin page url

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "https://admin-wow-wraps-tsx.vercel.app",
     "https://client-wow-wraps-tsx.vercel.app",
+    "https://wow-wrap-admin-v2-8xopyzjwn-huydayne168s-projects.vercel.app/",
     "http://localhost:3000",
     "http://localhost:3001",
 ];
