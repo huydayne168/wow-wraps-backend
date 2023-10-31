f8a7a0044741b3fc6a28f6c91ad39da350c6d75e
huydayne <huynguyenviet1608@gmail.com>
2023-10-31 20:33:38 +0700

    fix cors

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "https://admin-wow-wraps-tsx.vercel.app",
     "https://client-wow-wraps-tsx.vercel.app",
-    "https://wow-wrap-admin-v2-8xopyzjwn-huydayne168s-projects.vercel.app/",
+    "https://wow-wrap-admin-v2.vercel.app",
     "http://localhost:3000",
     "http://localhost:3001",
 ];
